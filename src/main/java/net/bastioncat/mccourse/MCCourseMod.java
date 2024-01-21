d7fa6b8aa3280aa51740337c9c98525ff3862dd1
sebastiancat <sebastianballcamelot@gmail.com>
2024-01-21 18:12:34 -0500

    Added orichalcum ingot and nugget

--- src/main/java/net/bastioncat/mccourse/MCCourseMod.java
+++ src/main/java/net/bastioncat/mccourse/MCCourseMod.java
@@ -1,5 +1,6 @@
 package net.bastioncat.mccourse;
 
+import net.bastioncat.mccourse.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ public class MCCourseMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final String Mod_ID = "mccourse";
     public static final Logger LOGGER = LoggerFactory.getLogger(Mod_ID);
 
@@ -18,6 +20,7 @@ public class MCCourseMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/bastioncat/mccourse/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.bastioncat.mccourse.item;
+
+import net.bastioncat.mccourse.MCCourseMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item ORICHALCUM_INGOT = registerItem("orichalcum_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item ORICHALCUM_NUGGET = registerItem("orichalcum_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(MCCourseMod.Mod_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod Items for " + MCCourseMod.Mod_ID);
+    }
+}
