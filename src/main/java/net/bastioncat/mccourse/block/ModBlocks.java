68c7ddc8b8e6bab6b0a74b6ca2202809320a3b34
sebastiancat <sebastianballcamelot@gmail.com>
2024-02-17 21:18:30 -0500

    Added Speedyblock

--- src/main/java/net/bastioncat/mccourse/block/ModBlocks.java
+++ src/main/java/net/bastioncat/mccourse/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.bastioncat.mccourse.block;
 
 import net.bastioncat.mccourse.MCCourseMod;
+import net.bastioncat.mccourse.block.custom.SpeedyBlock;
 import net.bastioncat.mccourse.item.ModItemGroups;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -15,16 +16,19 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
     public static final Block ORICHALCUM_BLOCK = registerBlock("orichalcum_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6.0f).requiresTool()), ModItemGroups.COURSE);
 
     public static final Block ORICHALCUM_ORE = registerBlock("orichalcum_ore",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroups.COURSE);
 
     public static final Block RAW_ORICHALCUM_BLOCK = registerBlock("raw_orichalcum_block",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(5f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool()), ModItemGroups.COURSE);
 
     public static final Block DEEPSLATE_ORICHALCUM_ORE = registerBlock("deepslate_orichalcum_ore",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(5f).requiresTool()), ModItemGroups.COURSE);
+            new Block(FabricBlockSettings.of(Material.STONE).strength(5.0f).requiresTool()), ModItemGroups.COURSE);
+
+    public static final Block SPEEDY_BLOCK = registerBlock("speedy_block",
+            new SpeedyBlock(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool()), ModItemGroups.COURSE);
 
 
 
