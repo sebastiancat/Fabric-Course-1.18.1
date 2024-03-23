38a03ed636352a83e557e0835d6c8ea85ab19afc
sebastiancat <sebastianballcamelot@gmail.com>
2024-03-22 23:07:21 -0400

    Added orichalcum armour

--- src/main/java/net/bastioncat/mccourse/item/ModItems.java
+++ src/main/java/net/bastioncat/mccourse/item/ModItems.java
@@ -3,6 +3,8 @@ package net.bastioncat.mccourse.item;
 import net.bastioncat.mccourse.MCCourseMod;
 import net.bastioncat.mccourse.item.custom.*;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ShovelItem;
 import net.minecraft.util.Identifier;
@@ -47,6 +49,23 @@ public class ModItems {
 
     public static final Item ORICHALCUM_PAXEL = registerItem("orichalcum_paxel",
             new ModPaxelItem(ModToolMaterial.ORICHALCUM, 1, 1f,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+
+    public static final Item ORICHALCUM_HELMET = registerItem("orichalcum_helmet",
+            new ArmorItem(ModArmorMaterials.ORICHALCUM, EquipmentSlot.HEAD,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    public static final Item ORICHALCUM_CHESTPLATE = registerItem("orichalcum_chestplate",
+            new ArmorItem(ModArmorMaterials.ORICHALCUM, EquipmentSlot.CHEST,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    public static final Item ORICHALCUM_LEGGINGS = registerItem("orichalcum_leggings",
+            new ArmorItem(ModArmorMaterials.ORICHALCUM, EquipmentSlot.LEGS,
+                    new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+    public static final Item ORICHALCUM_BOOTS = registerItem("orichalcum_boots",
+            new ArmorItem(ModArmorMaterials.ORICHALCUM, EquipmentSlot.FEET,
                     new FabricItemSettings().group(ModItemGroups.COURSE)));
 
 
