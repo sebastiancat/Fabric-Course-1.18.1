eed235e3c1e683eca55131fdfa0dd7fa65b5c7fb
sebastiancat <sebastianballcamelot@gmail.com>
2024-04-06 22:42:24 -0400

    Changed data tablet to have on and off textures

--- src/main/java/net/bastioncat/mccourse/MCCourseClient.java
+++ src/main/java/net/bastioncat/mccourse/MCCourseClient.java
@@ -1,6 +1,7 @@
 package net.bastioncat.mccourse;
 
 import net.bastioncat.mccourse.block.ModBlocks;
+import net.bastioncat.mccourse.util.ModModelPredicateProvider;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
@@ -10,5 +11,8 @@ public class MCCourseClient implements ClientModInitializer {
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CHERRY_BLOSSOM_DOOR, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CHERRY_BLOSSOM_TRAPDOOR, RenderLayer.getCutout());
+
+
+        ModModelPredicateProvider.registerModModels();
     }
 }
